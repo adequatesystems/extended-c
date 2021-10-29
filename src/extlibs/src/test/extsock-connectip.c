b031419b5333ed37176fd38f58f15e10d613ebda
Chrisdigity <chrisdigity@gmail.com>
2021-10-29 15:27:31 +1000

    try testing for socket error

--- src/extlibs/src/test/extsock-connectip.c
+++ src/extlibs/src/test/extsock-connectip.c
@@ -5,6 +5,8 @@
 
 int main()
 {  /* check connectip() can connect to listening sockets */
+   SOCKET sd = connectaddr("localhost", 54321);
+   if (sd == INVALID_SOCKET) ASSERT_EQ(getsockerr(), 0);
    ASSERT_EQ_MSG(connectaddr("localhost", 54321), INVALID_SOCKET,
       "connection should timeout, before listener is created");
    system("nc -l 54321 &");  /* creates listener in background */
