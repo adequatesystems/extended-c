799144c37e0cdb5bcd4ffdfd5f8dcdaf7b4d3cf0
Chrisdigity <chrisdigity@gmail.com>
2021-10-29 15:43:21 +1000

    debug socket descriptor

--- src/extlibs/src/test/extsock-blocking-nonblock.c
+++ src/extlibs/src/test/extsock-blocking-nonblock.c
@@ -7,7 +7,10 @@ int main()
 {  /* check connectip() can connect to listening sockets */
    SOCKET sd;
    system("nc -l 54321 &");  /* so connection succeeds */
-   ASSERT_NE_MSG((sd = connectaddr("localhost", 54321)), INVALID_SOCKET,
+   sd = connectaddr("localhost", 54321);
+   ASSERT_EQ_MSG(sd, sd, "WHAT IS SOCKET DESCRIPTOR!!!");
+   ASSERT(getsockerr() && "WHAT IS getsockerr()!!!");
+   ASSERT_NE_MSG(sd, INVALID_SOCKET,
       "connection failed, but needs to succeed to test [non]blocking()");
    ASSERT_EQ_MSG(blocking(sd), 0, "should set socket to blocking");
    ASSERT_EQ_MSG(nonblock(sd), 0, "should set socket to nonblocking");
