9f0158bb40d05242b5370d7546222ca1265b3821
Chrisdigity <chrisdigity@gmail.com>
2021-10-29 15:03:37 +1000

    remove EXCESSIVE debug output in random number tests

--- src/extlibs/src/test/extint-rand16-seeded.c
+++ src/extlibs/src/test/extint-rand16-seeded.c
@@ -4,12 +4,18 @@
 
 int main()
 {  /* check; random numbers do not exceed WORD16_MAX, and final seed */
-   word32 iterations = WORD32_C(0x12345678);
-   word32 expect = WORD32_C(0xadafae9f);
+   word32 iterations = WORD16_MAX;
+   word32 expect = WORD32_C(0x18b1e5c0);
    word32 seedA, seedB, seedC;
 
    srand16(iterations, 0, 0);
-   while (iterations--) ASSERT_LE(rand16(), WORD16_MAX);
+   while (iterations--) {
+      if (rand16() > WORD16_MAX) {
+         get_rand16(&seedA, &seedB, &seedC);
+         ASSERT_EQ2_MSG(seedA, seedB, seedC,
+            "the resulting seeds resulted in rand16() > WORD16_MAX");
+      }
+   }
    get_rand16(&seedA, &seedB, &seedC);
-   ASSERT_EQ(seedA + seedB + seedC, expect);
+   ASSERT_EQ((word32) (seedA + seedB + seedC), expect);
 }
--- src/extlibs/src/test/extint-rand16fast-seeded.c
+++ src/extlibs/src/test/extint-rand16fast-seeded.c
@@ -4,10 +4,15 @@
 
 int main()
 {  /* check; random numbers do not exceed WORD16_MAX, and final seed */
-   word32 iterations = WORD32_C(0x12345678);
-   word32 expect = WORD32_C(0x73d207a0);
+   word32 iterations = WORD16_MAX;
+   word32 expect = WORD32_C(0x19eb1f6);
 
    srand16fast(iterations);
-   while (iterations--) ASSERT_LE(rand16fast(), WORD16_MAX);
+   while (iterations--) {
+      if (rand16fast() > WORD16_MAX) {
+         ASSERT_LE_MSG(get_rand16fast(), WORD16_MAX,
+            "the resulting seed resulted in rand16fast() > WORD16_MAX");
+      }
+   }
    ASSERT_EQ(get_rand16fast(), expect);
 }
