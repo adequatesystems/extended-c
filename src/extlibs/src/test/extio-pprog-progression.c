8a995bb9757d1ff5df5d37c48672dcb7f3be71a7
Chrisdigity <chrisdigity@gmail.com>
2021-11-08 10:13:02 +1000

    update extio tests for pprog()

---
+++ src/extlibs/src/test/extio-pprog-progression.c
@@ -0,0 +1,25 @@
+
+#include "_assert.h"
+#include "../extio.h"
+#include <time.h>
+
+int main()
+{  /* this test simply tests basic operation without issue */
+   char prog_name[] = "Progress";
+   char prog_name2[] = "Progress2";
+   time_t now, then;
+   double i;
+
+   time(&then);
+   Pconsole = PCONSOLE_LOG;
+   /* print progress bar over 5 seconds, lazy spin loops */
+   pprog(prog_name, "B", 0, 0);
+   pprog(prog_name2, NULL, 0, 5);
+   for (i = 0; i < 5; time(&now), i = difftime(now, then)) {
+      pprog(prog_name, NULL, i, 0);
+      pprog(prog_name2, NULL, i, 5);
+      i++;
+   }
+   pprog_done(prog_name);
+   pprog_done(prog_name2);
+}
