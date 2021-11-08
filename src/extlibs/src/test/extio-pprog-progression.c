fea7f6d7ec512d67cd90015bde5b86e750c94e33
Chrisdigity <chrisdigity@gmail.com>
2021-11-08 10:19:21 +1000

    remove erroneous printing in pprog() test

--- src/extlibs/src/test/extio-pprog-progression.c
+++ src/extlibs/src/test/extio-pprog-progression.c
@@ -7,18 +7,19 @@ int main()
 {  /* this test simply tests basic operation without issue */
    char prog_name[] = "Progress";
    char prog_name2[] = "Progress2";
-   time_t now, then;
+   time_t last, now, then;
    double i;
 
-   time(&then);
+   last = time(&then);
    Pconsole = PCONSOLE_LOG;
    /* print progress bar over 5 seconds, lazy spin loops */
    pprog(prog_name, "B", 0, 0);
    pprog(prog_name2, NULL, 0, 5);
    for (i = 0; i < 5; time(&now), i = difftime(now, then)) {
-      pprog(prog_name, NULL, i, 0);
-      pprog(prog_name2, NULL, i, 5);
-      i++;
+      if (difftime(now, last)) {
+         pprog(prog_name, NULL, i, 0);
+         pprog(prog_name2, NULL, i, 5);
+      }
    }
    pprog_done(prog_name);
    pprog_done(prog_name2);
